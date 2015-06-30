2e60ba2bd60b881b2aa28274fec16b0b506efb35
ronrest <ronny.coding@gmail.com>
2015-06-30 20:01:00 +1000

    add diri() function

--- ronnys/__init__.py
+++ ronnys/__init__.py
@@ -1,1 +1,22 @@
-print('Hey there!')
+def diri(obj, s="", internals=False):
+    """
+    Is like dir() function, but allows you to filter for values containing some
+    text.
+
+    If only the first argument is provided, then it behaves pretty much the same
+    way that dir() behaves, only it prints every item in a new line, and hides
+    internal values.
+
+    :param obj: (object)
+    :param s: (string)
+    :param internals: (boolean) should it show internal methods and values?
+                      ie, values begining with "_" or "__"?
+
+                      (DEFAULT is False)
+    """
+    for item in dir(obj):
+        if (item.startswith("_") and not internals):
+            continue
+        if s in item:
+            print(item)
+
